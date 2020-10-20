f5791942e11eae96c61d0b07b82f9e7b09ca3c27
Oleksandr_Baglai <apofig@gmail.com>
2020-10-20 10:01:25 +0300

    [pseudo][bomberman] Исправил вывод Rule что отработала

--- codenjoy/games/bomberman/src/main/pseudo/0-settings.bat
+++ codenjoy/games/bomberman/src/main/pseudo/0-settings.bat
@@ -1,8 +1,8 @@
 set GAME_TO_RUN=bomberman
 set BOARD_URL=https://dojorena.io/codenjoy-contest/board/player/playerId?code=12345678901234567890
 
-set JAVA_HOME=%CD%\tools\jdk
-set M2_HOME=%CD%\tools\mvnw
+set JAVA_HOME=%CD%\jdk8
+set M2_HOME=%CD%\maven
 set APP_HOME=%CD%\sources\%GAME_TO_RUN%
 set ENGINE_HOME=%CD%\sources\engine
 set RULES_HOME=%CD%\rules
